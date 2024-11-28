7e66c0d22c3774fb28fd38ccdea903ec494abc7f
victor-gabryel <victorgabryel2123@gmail.com>
2024-11-28 14:08:40 -0300

    versao_08

--- script/script.js
+++ script/script.js
@@ -9,21 +9,18 @@ function mode(){
         document.body.style.backgroundColor = "#141414"; //mudar cor
     }
 }
-
 function AbrirLateralMenu() {
     var menu = document.querySelector('.menu-lateral');
     var navBar = document.querySelector('.nav-bar');
     // Alterna a visibilidade do menu lateral
     menu.style.left = '0px';     // Exibe o menu
 }
-
 function FecharLateralMenu() {
     var menu = document.querySelector('.menu-lateral');
     var navBar = document.querySelector('.nav-bar');
     // Alterna a visibilidade do menu lateral
     menu.style.left = '-250px';     // Exibe o menu
 }
-
 // Fecha o menu automaticamente quando a tela for maior que 768px
 window.addEventListener('resize', function() {
     var menu = document.querySelector('.menu-lateral');
@@ -33,15 +30,6 @@ window.addEventListener('resize', function() {
 });
 
 
-//index========================================================================================================
-
-
-
-
-
-
-
-
 
 //galeria======================================================================================================
 // Função para mover o carrossel
@@ -49,12 +37,10 @@ function moverCarrossel(usuarioIndex, direcao) {
     const carrossel = document.getElementById(`carrossel-${usuarioIndex}`);
     const cards = carrossel.getElementsByClassName('card');
     const numeroDeCards = cards.length;
-
     // Pegue o primeiro ou último cartão visível
     const cardWidth = cards[0].offsetWidth + 20; // largura de um card mais a margem
     const currentTransform = carrossel.style.transform;
     const currentPosition = currentTransform ? parseInt(currentTransform.split('(')[1].split('px')[0]) : 0;
-
     // Calcular o novo deslocamento (offset)
     let novoDeslocamento;
     if (direcao === 'next') {
@@ -70,7 +56,6 @@ function moverCarrossel(usuarioIndex, direcao) {
         novoDeslocamento = -(cardWidth * (numeroDeCards - 1));
       }
     }
-
     // Aplica a nova transformação
     carrossel.style.transform = `translateX(${novoDeslocamento}px)`;
 }
@@ -78,7 +63,6 @@ function moverCarrossel(usuarioIndex, direcao) {
 
 
 //feedback=====================================================================================================
-// Banco de dados simulado com um array de objetos
 const bancoDeDados = [
     { email: "andreas@gmail.com", senha: "123456" },
     { email: "gabriel@gmail.com", senha: "123456" },
@@ -104,7 +88,6 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     if (usuario) {
         alert("Login bem-sucedido!");
         console.log("Usuário logado:", usuario.email);
-        // Aqui você pode redirecionar para outra página ou realizar outras ações
     } else {
         alert("Usuário não encontrado ou senha incorreta.");
     }
@@ -112,10 +95,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
 // Função para "Esqueci a senha"
 document.getElementById("forgotPassword").addEventListener("click", function() {
-    alert("Erro: Funcionalidade de recuperação de senha não implementada.");
+    alert("[Erro]...");
 });
 
 // Função para "Cadastrar-se"
 document.getElementById("signUpLink").addEventListener("click", function() {
-    alert("Erro: Funcionalidade de cadastro não implementada.");
+    alert("[Erro]...");
 });
